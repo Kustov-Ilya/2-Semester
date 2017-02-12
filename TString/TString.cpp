0a7eae891e391705516aa4a027d2fe8d0a231888
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-12 18:15:28 +0300

    1234

---
+++ TString/TString.cpp
@@ -0,0 +1,233 @@
+#include "TString.h"
+
+
+TString::~TString()
+{
+	delete[] Data;
+}
+
+
+TString::TString()
+{
+	Data = new char[1];
+	Data[0] = '\0';
+}
+
+
+TString::TString(const TString& rhs)
+{
+	size_t Len = rhs.Size();
+	Data = new char[Len + 1];
+	for (int i = 0; i < Len; i++)
+	{
+		Data[i] = rhs.Data[i];
+	}
+	Data[Len] = '\0';
+}
+
+
+TString::TString(const char * data)
+{
+	size_t Len = 0;
+	while (data[Len] != '\0')
+	{
+		Len++;
+	}
+	Data = new char[Len + 1];
+	for (int i = 0; i < Len; i++)
+	{
+		Data[i] = data[i];
+	}
+	Data[Len] = '\0';
+}
+
+
+TString& TString::operator =(const TString& rhs)
+{
+	if (rhs == *this) return *this;
+	if(Data != nullptr) delete[] Data;
+	size_t Len = rhs.Size();
+	Data = new char[Len + 1];
+	for (int i = 0; i < Len; i++) Data[i] = rhs.Data[i];
+	Data[Len] = '\0';
+	return *this;
+}
+
+
+TString& TString::operator +=(const TString& rhs)
+{
+	size_t Len1= TString(Data).Size();
+	size_t Len2 = rhs.Size();
+	size_t Len = Len1 + Len2;
+	char * Data1 = new char[Len + 1];
+	for (int i = 0; i < Len1; i++)
+	{
+		Data1[i] = Data[i];
+	}
+	for (int i = 0; i < Len2; i++)
+	{
+		Data1[Len1 + i] = rhs.Data[i];
+	}
+	delete[] Data;
+	Data = Data1;
+	Data[Len] = '\0';
+	return *this;
+}
+
+
+bool TString::operator ==(const TString& rhs) const
+{
+	size_t Len = TString(Data).Size();
+	if (Len != rhs.Size()) return false;
+	for (int i = 0; i < Len; i++)
+	{
+		if (Data[i] != rhs.Data[i]) return false;
+	}
+	return true;
+}
+
+
+bool TString::operator <(const TString& rhs) const
+{
+	int length;
+	if (TString(rhs.Data).Size() > TString(this->Data).Size())
+		length = TString(rhs.Data).Size();
+	else
+		length = TString(Data).Size();
+	for (int i = 0; i<length; i++){
+		if (Data[i] == rhs.Data[i]) continue;
+		if (Data[i]<rhs.Data[i]) return true;
+		if (Data[i]>rhs.Data[i]) return false;
+	}
+	return false;
+}
+
+
+size_t TString::Find(const TString& substr) const{
+	int Len1 = TString(Data).Size();
+	int Len2 = TString(substr.Data).Size();
+	if (Len2 > Len1) return -1;
+	int chek, t = 0, i = 0;
+	for (i; i < Len1 - Len2 + 1; i++){
+		t = i;
+		for (int j = 0; j < Len2; j++)
+		{
+			if (Data[t] == substr.Data[j])
+			{
+				t++;
+				chek = 1;
+				continue;
+			}
+			else
+			{
+				chek = 0;
+				break;
+			}
+		}
+		if (chek == 1) return i;
+	}
+	return -1;
+}
+
+
+void TString::Replace(char oldSymbol, char newSymbol)
+{
+	for (int i = 0; i < TString(Data).Size(); i++)
+		if (Data[i] == oldSymbol)
+		{
+			Data[i] = newSymbol;
+		}
+}
+
+
+size_t TString::Size() const
+{
+	size_t i = 0;
+	for (i; Data[i]; i++);
+	return i;
+}
+
+
+bool TString::Empty() const
+{
+	if (Data[0]==NULL) return true;
+	return false;
+}
+
+
+char TString::operator[](size_t index) const
+{
+	return Data[index];
+}
+
+
+char& TString::operator[](size_t index)
+{
+	return Data[index];
+}
+
+
+void TString::RTrim(char symbol)
+{
+	int Len = TString(Data).Size();
+	int t = 0;
+	if (Data[Len - 1 - t] == symbol)
+	{
+		t++;
+	}
+	char* Data1 = new char[Len - t + 1];
+	for (int i = 0; i < Len - t; i++)
+	{
+		Data1[i] = Data[i];
+	}
+	Data1[Len - t] = '\0';
+	delete[] Data;
+	Data = Data1;
+}
+
+
+void TString::LTrim(char symbol)
+{
+	int Len = TString(Data).Size();
+	int t = 0;
+	if (Data[t] == symbol)
+	{
+		t++;
+	}
+	char * Data1 = new char[Len - t + 1];
+	for (int i = 0; i < Len - t; i++)
+	{
+		Data1[i] = Data[i + t];
+	}
+	Data1[Len - t] = '\0';
+	delete[] Data;
+	Data = Data1;
+}
+
+
+TString operator+(const TString & a, const TString & b)
+{
+	return (TString(a) += TString(b));
+}
+
+
+bool operator!=(const TString & a, const TString & b)
+{
+	if (a == b) return false;
+	return true;
+}
+
+
+bool operator>(const TString & a, const TString & b)
+{
+	if (a == b) return false;
+	if (a < b) return false;
+	return true;
+}
+
+
+std::ostream & operator<<(std::ostream &out, const TString &str)
+{
+	out << str.Data;
+	return out;
+}
