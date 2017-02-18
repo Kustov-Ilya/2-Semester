52e098654fc9ef46c2f4e4113da4607fbe145d44
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-18 18:49:22 +0300

    Create TClass.cpp

---
+++ LW_2/TClass.cpp
@@ -0,0 +1,24 @@
+#include "TClass.h"
+
+
+THaSoPr::~THaSoPr() {}
+
+
+THaSoPr::THaSoPr() {}
+
+
+THaSoPr::THaSoPr(const std::string & nName, const std::size_t & nNomber){
+	Name = nName;
+	Nomber = nNomber;
+}
+
+
+void THaSoPr::Print(){
+	std::cout << "Name is " << Name << ", class is " << Nomber << std::endl;
+}
+
+
+bool THaSoPr::Check(const std::size_t & nNomber){
+	if (Nomber <= nNomber) return true;
+	return false;
+}
