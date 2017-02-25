238ded989f65af35b47008ed546ff0610c9292d9
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-25 17:26:12 +0300

    Update Head.h

--- LW_7/LW_7_2/Head.h
+++ LW_7/LW_7_2/Head.h
@@ -10,15 +10,15 @@ struct Text{
 	Text *prev;
 };
 
-//Инициализания структуры
+//Инициализания списка
 int InitText(Text **Head);
 
-//Добавление слова в структуру
+//Добавление слова в список
 int AddWord(Text **Head, Text *word);
 
 
 //Поиск и вывод слова str в контексте интервала n
 void Check(Text *Head, const string str, const int n);
 
-//Удаление структуры
+//Удаление списка структур
 void DeleteText(Text **Head);
