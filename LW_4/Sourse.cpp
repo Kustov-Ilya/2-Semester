ea0775d9c716ede963b88307a2a5dbb4617b0315
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-25 18:45:55 +0300

    Create Sourse.cpp

---
+++ LW_4/Sourse.cpp
@@ -0,0 +1,16 @@
+#include"Class.h"
+
+int main(){
+	RealNum Num(-6.345);
+	RealNum Nu;
+	ComplexNum Nu1;
+	ComplexNum NumC(5.345, -4.567);
+	ComplexNum NumC1(5.345, 6.967);
+	Num.Print();
+	NumC.Print();
+	NumC1.Print();
+	Nu.Print();
+	Nu1.Print();
+	system("pause");
+	return 0;
+}
