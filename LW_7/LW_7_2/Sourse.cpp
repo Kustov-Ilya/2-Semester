ddf8e2c4b185c70bca6452d2e859f21745370c95
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-26 02:03:34 +0300

    Update Sourse.cpp

--- LW_7/LW_7_2/Sourse.cpp
+++ LW_7/LW_7_2/Sourse.cpp
@@ -1,40 +1,121 @@
-#include"Head.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+struct Text{
+	std::string Wor;
+	Text *next;
+	Text *prev;
+};
+
+int AddWord(Text **Head, std::string str){
+	if (*Head == nullptr){
+		*Head = new Text;
+		(*Head)->Wor = str;
+		(*Head)->next = nullptr;
+		(*Head)->prev = nullptr;
+		return 1;
+	}
+	Text *Now= *Head;
+	while (Now->next!=nullptr)
+	{
+		Now = Now->next;
+	}
+	Text *tmp = new Text;
+	Now->next = tmp;
+	tmp->Wor = str;
+	tmp->prev = Now;
+	tmp->next = nullptr;
+	return 1;
+}
+
+Text *Break(std::string str){
+	size_t End = str.length()-1;
+	size_t Poz = 0;
+	size_t EndPoz = 0;
+	Text *pH = nullptr;
+	//char Spase = ' ';
+	while (EndPoz <= End){
+		EndPoz = str.find(' ', Poz);
+		AddWord(&pH, str.substr(Poz, EndPoz - Poz));
+		Poz = EndPoz+1;
+	}
+	return pH;
+}
+
+void Clear(Text**Head){
+	while(*Head!=nullptr){
+		Text *tmp = *Head;
+		(*Head) = (*Head)->next;
+		delete tmp;
+	}
+}
+
+int FindAndOut(Text *Head, const std::string str, const int n){
+	bool Che = false;
+	while (Head != NULL){			//Поиск str слов в структуре. При нахождении
+		if (Head->Wor == str){		//нахождение и вывод интервала n
+			int k = n;
+			Text *Now = Head;
+			int i = 0;
+			while (i<k && Now->prev != NULL){
+				Now = Now->prev;
+				i++;
+			}
+			k += i;
+			int t = 0;
+			while (t <= k && Now != NULL){
+				std::cout << " " << Now->Wor;
+				Now = Now->next;
+				if (t == i) std::cout << "(!!!)"; //Выделение слова, которое нашли
+				t++;
+			}
+			std::cout << std::endl;
+			Che = true;
+		}
+		Head = Head->next;
+	}
+	if (Che == true) return 1;
+	return 0;
+}
+
+std::string ReadString(std::fstream & file){
+	std::string line;
+	std::getline(file, line);
+	return line;
+}
+
+int GetN(){
+	int n=-1;
+	while (n < 0){
+		std::cout << "Enter n: ";
+		std::cin >> n;
+		std::cin.clear();
+		std::cin.sync();
+	}
+	return n;
+}
+
+std::string GetWordFind(){
+	std::string WordFind;
+	std::cout << "Enter word: ";
+	std::cin >> WordFind;
+	return WordFind;
+}
 
 int main(){
-	Text *pF;//указатель на первый эллемент список структур
-	InitText(&pF);
-	int n = 0;
-	string WordFind;
-	string Word;
-	char buf[128];
-	cout << "Enter word: ";
-	cin >> buf;
-	WordFind = buf;
-	while (n < 1){
-		cout << "Enter n: ";
-		cin >> n;
-		cin.clear();
-		cin.sync();
+	std::fstream file("Text.txt", std::ios::in);
+	if (!file.is_open()) return 0;
+	std::string WordFind = GetWordFind();
+	int n = GetN();
+	int Check = 0;
+	while (!file.eof()){
+		Text *pF = Break(ReadString(file));
+		Check += FindAndOut(pF, WordFind, n);
+		Clear(&pF);
 	}
-	ifstream file;
-	file.open("Text.txt", ios::in);
-	if (!file) {
-		cout << "File error - can't open to read data!";
-		cin.sync();
-		cin.get();
-		return 2;
-	}
-	while (1) {					//Чтение файла и добавление слов в список
-		if (file.eof()) break;
-		file >> buf;
-		Word = buf;
-		Text *Wor = new Text;
-		Wor->Wor = Word;
-		AddWord(&pF, Wor);
-	}
+	if (Check == 0) std::cout << "Word don't found" << std::endl;
 	file.close();
-	Check(pF, WordFind, n);
-	DeleteText(&pF);
 	system("pause");
 	return 0;
 }
