d50f04f97256fc7eaff144260aa224382dfe8a92
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-27 15:27:51 +0300

    Update ComplexNum.h

--- LW_4/ComplexNum.h
+++ LW_4/ComplexNum.h
@@ -1,17 +1,28 @@
 #include"RealNum.h"
 
-class ComplexNum : protected RealNum{
+class ComplexNum : public RealNum{
 protected:
 	double NumI;
 public:
-	~ComplexNum() = default;
 
-	ComplexNum();
+	ComplexNum()
+		:RealNum(0), NumI(0){}
 
 	ComplexNum(double num, double numi)
-		:RealNum(num), NumI(numi){};
+		:RealNum(num), NumI(numi){}
 
-	virtual double Modul();
+	virtual double Modul(){
+		return sqrt(pow(Num, 2) + pow(NumI, 2));
+	}
 
-	virtual void Print();
+	virtual void Print(){
+		cout << "ComplexNum: " << Num << " + i*";
+		if (NumI > 0) {
+			cout << NumI;
+		}
+		else{
+			cout << "(" << NumI << ")";
+		}
+		cout << " \nModul of ComplexNum: " << Modul() << endl;
+	}
 };
