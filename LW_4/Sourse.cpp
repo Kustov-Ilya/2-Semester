1d3a2fc013ce691ec04fa11134693d03d340133c
Kustov-Ilya <kustoff.il@yandex.ru>
2017-02-27 15:26:56 +0300

    Update Sourse.cpp

--- LW_4/Sourse.cpp
+++ LW_4/Sourse.cpp
@@ -2,16 +2,15 @@
 #include"ComplexNum.h"
 
 int main(){
-	RealNum Num(-6.345);
-	RealNum Nu;
-	ComplexNum Nu1;
-	ComplexNum NumC(5.345, -4.567);
-	ComplexNum NumC1(5.345, 6.967);
-	Num.Print();
-	NumC.Print();
-	NumC1.Print();
-	Nu.Print();
-	Nu1.Print();
+	RealNum *Real = new RealNum(-6.345);
+	RealNum *Complex1 = new ComplexNum(5.345, 6.967);
+	ComplexNum *Complex2 = new ComplexNum(4.324, -4.322);
+	Real->Print();
+	Complex1->Print(); 
+	Complex2->Print();
+	delete Real;
+	delete Complex1;
+	delete Complex2;
 	system("pause");
 	return 0;
 }
