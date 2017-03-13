097d613ede1650b098ae83c5b9968d6090d244a7
Kustov-Ilya <kustoff.il@yandex.ru>
2017-03-13 17:54:01 +0300

    Update Header.h

--- M_2_LW_1/Header.h
+++ M_2_LW_1/Header.h
@@ -1,15 +1,28 @@
 #pragma once
-
+#include<iostream>
 #include <exception>
 
 class Exception
 	: public std::exception
-{};
+{
+public:
+	virtual void what()=0;
+};
 
 class BigLen
 	: public Exception
-{};
+{
+public:
+	void what() {
+		std::cout << "BigLen" << std::endl;
+	}
+};
 
 class UnknownValue
 	: public Exception
-{};
+{
+public:
+	void what() {
+		std::cout << "UnknownValue" << std::endl;
+	}
+};
