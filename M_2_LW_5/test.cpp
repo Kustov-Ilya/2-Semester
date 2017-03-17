cf3b99ecbf3d3f881e2b211fa7672ae4366c9847
Kustov-Ilya <kustoff.il@yandex.ru>
2017-03-17 19:32:24 +0400

    Add files via upload

---
+++ M_2_LW_5/test.cpp
@@ -0,0 +1,23 @@
+#include "TNotCopyable.h"
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		TNotCopyable obj;
+
+		RAII obiadr(&obj);
+
+		obj.OpenToWrite("someFile.txt");
+
+		obj.OpenToRead("someOtherFile.txt");
+
+		obj.Close();
+	}
+	catch (const std::exception & e)
+	{
+		std::cout << e.what();
+	}
+	system("pause");
+}
